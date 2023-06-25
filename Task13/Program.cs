27526c24cd1a66535f1e0306144ec92f9046a400
PolinaKulicchkova <polya841345@gmail.com>
2023-06-25 23:57:33 +0300

    Added Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100) {
+    while (number > 999) {
+        number = number / 10;
+    }
+    number = number % 10;
+}
+if (number < 10) {
+    Console.WriteLine("Третья цифра числа: " + number);
+} else {
+    Console.WriteLine("Третьей цифры нет");
+}
